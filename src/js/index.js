2b81eea96650e3c212829cb123fe18753ecc7e41
vladimirovicp <vladimirovicp70@gmail.com>
2022-06-06 17:12:57 +0400

    add: header

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+window.addEventListener('DOMContentLoaded', () =>{
+    let arrowTel = document.querySelector('.header__arrow');
+    let arrowTelephone = document.querySelector('.header__arrow-one');
+    let arrowTelephoneHidden = document.querySelector('.header__arrow-two');
+    let telephoneHeader = document.querySelector('.header__number-two');
+
+    arrowTel.addEventListener('click', (e) => {
+        const target = e.target.closest('.header__arrow-one');
+        const targetTwo = e.target.closest('.header__arrow-two');
+
+        // Element.closest()
+        // Метод Element.closest() возвращает ближайший родительский элемент (или сам элемент), который соответствует заданному CSS-селектору или null, если таковых элементов вообще нет.
+
+        if (target){
+            telephoneHeader.style.display = 'block';
+            arrowTelephone.style.display = 'none';
+            arrowTelephoneHidden.style.display = 'block';
+        }
+        if (targetTwo){
+
+            telephoneHeader.style.display = 'none';
+            arrowTelephone.style.display = 'block';
+            arrowTelephoneHidden.style.display = 'none';
+        }
+        // console.log(target);
+    });
+});
